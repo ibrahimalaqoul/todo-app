d434f97bf90cd3e8e2e8686306fb351a9136bf0e
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-05-16 22:15:50 +0300

    unfinished context

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import ToDo from './components/todo/todo';
+
+function App() {
+  return (
+    <div className="App">
+         <ToDo />
+
+    </div>
+  );
+}
+
+export default App;
